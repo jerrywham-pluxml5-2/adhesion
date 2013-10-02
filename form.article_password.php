a5efc48329c869b0a945056a8e42dc0439c6b765
Cyril MAGUIRE <github@ecyseo.net>
2013-10-02 09:32:11 +0200

    First push

---
+++ form.article_password.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * Plugin adhesion
+ *
+ * @version	1.2
+ * @date	02/10/2013
+ * @author	Stephane F, Cyril MAGUIRE
+ **/
+if(!defined('PLX_ROOT')) exit;
+ ?><form action="" method="post">
+	<fieldset>
+		<p><label>Mot de passe &nbsp;:</label><br /></p>
+		<input type="password" name="password" size="25" maxlength="50" value=" ">
+		<input type="hidden" name="lockArticles">
+		<input type="submit" value="ok" />
+	</fieldset>
+</form>
---
+++ form.forgetmypass.html.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * Plugin adhesion
+ *
+ * @version	1.2
+ * @date	02/10/2013
+ * @author	Stephane F, Cyril MAGUIRE
+ **/
+if(!defined('PLX_ROOT')) exit;
+ ?><form action="" method="post">
+	<fieldset>
+		<p><label>Votre adresse email &nbsp;:</label><br /></p>
+		<input type="text" name="email" size="25" maxlength="150" value="">
+		<input type="hidden" name="forgetmypass">
+		<input type="submit" value="ok" />
+	</fieldset>
+</form>
