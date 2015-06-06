86ce22ba17587d2069d9d0435a53ada50e64d1f5
mathieu269 <exemple@exemple.fr>
2015-06-06 23:35:59 +0200

    correction code

--- form.adherer.html.php
+++ form.adherer.html.php
@@ -5,12 +5,15 @@
  * @version	1.5
  * @date	07/10/2013
  * @author	Stephane F, Cyril MAGUIRE
- **/
- if(!defined('PLX_ROOT')) exit;
+**/
+
+if(!defined('PLX_ROOT')) exit;
 
 $plxMotor = plxMotor::getInstance();
 
-$plxPlugin = $plxShow->plxMotor->plxPlugins->aPlugins["adhesion"];
+$plxPlugin = $plxMotor->plxPlugins->aPlugins["adhesion"];
 
-?>
-<?php if ($plxPlugin->getParam('desc_adhesion') != str_replace("'","’",$plxPlugin->getLang('L_DEFAULT_DESC'))) echo htmlspecialchars_decode($plxPlugin->getParam('desc_adhesion'));?>
+
+if ($plxPlugin->getParam('desc_adhesion') != str_replace("'","’",$plxPlugin->getLang('L_DEFAULT_DESC'))) {
+	echo htmlspecialchars_decode($plxPlugin->getParam('desc_adhesion'));
+}
